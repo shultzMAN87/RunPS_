a6ae5e58e5f8f07dfe8954f41f3330c6badc3f3c
Pavlyushkevich.KE <zuker_gta@mail.ru>
2023-11-09 08:56:07 +1000

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import subprocess
-import sys
 
-p = subprocess.Popen(
-    'powershell.exe Start-Process -FilePath \'C:\Program Files\Crypto Pro\CSP\certmgr.exe\' -ArgumentList \'-list -store mmy\' -Wait -NoNewWindow',
-    stdout=sys.stdout,
-)
 
-p.communicate()
+arguments = [
+    'powershell.exe Start-Process',
+    '-FilePath \'C:\\Program Files\\Crypto Pro\\CSP\\certmgr.exe\'',
+    # Получение информации о сертификатах
+    # '-ArgumentList \'-list -store mmy\'',
+    # Удаление сертификата
+    '-ArgumentList \'-delete -store mmy -thumbprint a13f7d64728c95fcf31d57ceccd35acb173ed27e\'',
+    '-Wait',
+    '-NoNewWindow'
+]
+
+res = subprocess.Popen(' '.join(arguments), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+for line in res.stdout.readlines():
+    line = line.strip()
+    if line:
+        print(line.decode('cp866'))
